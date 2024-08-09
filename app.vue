31008f0e51634fba2388cd60d643344892ce47ef
bir-dv <brukjoseph@gmail.com>
2024-08-09 05:43:16 -0400

    update hero section and some designes

--- app.vue
+++ app.vue
@@ -1,33 +1,40 @@
 <template>
 
-  <LayoutNav />
+  <LayoutHero />
 
-  <!-- /////////////// -->
+  <LayoutServices />
+  <LayoutWhyus />
+
+  <section>
+    <div class="py-10 h-96  bg-cover rounded-lg cursor-pointer  ">
+
+      <video class="justify-center  object-cover w-full h-96 " autoplay loop muted playsinline
+        src="/assets/video/backgroundvideo.mp4"></video>
+      <div class="justify-center  -mt-40">
+        <h1 class="text-center text-4xl text-white font-['ethnocentric_rg'] uppercase ">Where Car Dreams Become <br>
+          Reality</h1>
+        <!-- <h1 class="text-center text-3xl text-white font-serif ">asdfghjklaqwer</h1> -->
+
+      </div>
 
 
+    </div>
+
+  </section>
   <section class=" bg-white dark:bg-gray-900">
     <div class="container px-6 py-10 mx-auto">
-      <div class="flex items-center py-4">
-        <!-- The left line -->
-        <div class="flex-grow h-px bg-[#fb8500]"></div>
 
-        <!-- Your text here -->
-        <span class=" flex-shrink text-6xl text-black px-4 italic font-bold"> <span
-            class="text-[#fb8500]">So</span>Cal</span>
-
-        <!-- The right line -->
-        <div class="flex-grow h-px bg-[#fb8500]"></div>
-      </div>
-
-      <div class="flex">
+      <div class="flex pt-20">
         <div class=" max-w-80 mt-5  flex-grow h-1 rounded  bg-[#fb8500]"></div>
-        <p class="text-2xl px-2  italic font-extralight ">VEHICLES</p>
+        <p class="text-2xl px-2  italic font-['ethnocentric_rg'] ">VEHICLES</p>
       </div>
 
 
-      <h1 class="pt-6 text-2xl font-serif text-gray-800  uppercase lg:text-7xl dark:text-white">Explore our
+      <h1 class="pt-6 text-2xl font-['ethnocentric_rg'] text-gray-800  uppercase lg:text-6xl dark:text-white">Explore
+        our
       </h1>
-      <h1 class="text-2xl font-serif  text-gray-800 uppercase lg:text-7xl dark:text-white">pristine exotics
+      <h1 class="text-2xl font-['ethnocentric_rg']  text-gray-800 uppercase lg:text-6xl dark:text-white">pristine
+        exotics
       </h1>
 
       <!-- /////////// -->
@@ -36,7 +43,7 @@
       <div class="container px-6 py-18 mx-auto">
 
         <div class="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-8 md:grid-cols-2 xl:grid-cols-3">
-          <div class="block overflow-hidden bg-cover rounded-lg cursor-pointer h-96 group"
+          <div @click="toggleModal" class="block overflow-hidden bg-cover rounded-lg cursor-pointer h-96 group"
             style="background-image:url('https://images.pexels.com/photos/13387265/pexels-photo-13387265.jpeg?auto=compress&cs=tinysrgb&w=400')">
             <div
               class="flex flex-col justify-center w-full h-full px-8 pt-60 transition-opacity duration-700 opacity-0 backdrop-blur-sm bg-gray-800/60 group-hover:opacity-100 ">
@@ -44,14 +51,14 @@
               <p class="mt-2 text-lg tracking-wider text-white  ">exuding elegance</p>
               <div class="flex">
                 <div class=" max-w-60 mt-5  flex-grow h-1 rounded  bg-[#fb8500]"></div>
-                <p class="text-2xl px-2  italic font-extralight text-[#fb8500] ">view collection</p>
+                <p class="text-2xl px-2  italic font-extralight text-[#fb8500] ">View Car</p>
               </div>
             </div>
 
 
           </div>
 
-          <div class="block overflow-hidden bg-cover rounded-lg cursor-pointer h-96 group"
+          <div @click="toggleModal" class="block overflow-hidden bg-cover rounded-lg cursor-pointer h-96 group"
             style="background-image:url('https://images.pexels.com/photos/2613276/pexels-photo-2613276.jpeg?auto=compress&cs=tinysrgb&w=400')">
             <div
               class="flex flex-col justify-center w-full h-full px-8 pt-60 transition-opacity duration-700 opacity-0 backdrop-blur-sm bg-gray-800/60 group-hover:opacity-100 ">
@@ -59,12 +66,12 @@
               <p class="mt-2 text-lg tracking-wider text-white  ">elegant design</p>
               <div class="flex">
                 <div class=" max-w-60 mt-5  flex-grow h-1 rounded  bg-[#fb8500]"></div>
-                <p class="text-2xl px-2  italic font-extralight text-[#fb8500] ">view collection</p>
+                <p class="text-2xl px-2  italic font-extralight text-[#fb8500] ">View Car</p>
               </div>
             </div>
           </div>
 
-          <div class="block overflow-hidden bg-cover rounded-lg cursor-pointer h-96 group"
+          <div @click="toggleModal" class="block overflow-hidden bg-cover rounded-lg cursor-pointer h-96 group"
             style="background-image:url('https://images.pexels.com/photos/9322199/pexels-photo-9322199.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')">
             <div
               class="flex flex-col justify-center w-full h-full px-8 pt-60 transition-opacity duration-700 opacity-0 backdrop-blur-sm bg-gray-800/60 group-hover:opacity-100 ">
@@ -72,7 +79,7 @@
               <p class="mt-2 text-lg tracking-wider text-white  ">unleash your inner beast</p>
               <div class="flex">
                 <div class=" max-w-60 mt-5  flex-grow h-1 rounded  bg-[#fb8500]"></div>
-                <p class="text-2xl px-2  italic font-extralight text-[#fb8500] ">view collection</p>
+                <p class="text-2xl px-2  italic font-extralight text-[#fb8500] ">View Car</p>
               </div>
             </div>
           </div>
@@ -83,7 +90,7 @@
       <div class="-mt-10 container px-6 mx-auto">
 
         <div class="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-8 md:grid-cols-2 xl:grid-cols-3">
-          <div class="block overflow-hidden bg-cover rounded-lg cursor-pointer h-96 group"
+          <div @click="toggleModal" class="block overflow-hidden bg-cover rounded-lg cursor-pointer h-96 group"
             style="background-image:url('https://images.pexels.com/photos/2568539/pexels-photo-2568539.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')">
             <div
               class="flex flex-col justify-center w-full h-full px-8 pt-60 transition-opacity duration-700 opacity-0 backdrop-blur-sm bg-gray-800/60 group-hover:opacity-100 ">
@@ -91,12 +98,12 @@
               <p class="mt-2 text-lg tracking-wider text-white  ">legendary performance</p>
               <div class="flex">
                 <div class=" max-w-60 mt-5  flex-grow h-1 rounded  bg-[#fb8500]"></div>
-                <p class="text-2xl px-2  italic font-extralight text-[#fb8500] ">view collection</p>
+                <p class="text-2xl px-2  italic font-extralight text-[#fb8500] ">View Car</p>
               </div>
             </div>
           </div>
 
-          <div class="block overflow-hidden bg-cover rounded-lg cursor-pointer h-96 group"
+          <div @click="toggleModal" class="block overflow-hidden bg-cover rounded-lg cursor-pointer h-96 group"
             style="background-image:url('https://images.pexels.com/photos/19189161/pexels-photo-19189161/free-photo-of-close-up-of-bentley-logo-with-raindrops-on-the-car.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')">
             <div
               class="flex flex-col justify-center w-full h-full px-8 pt-60 transition-opacity duration-700 opacity-0 backdrop-blur-sm bg-gray-800/60 group-hover:opacity-100 ">
@@ -104,12 +111,12 @@
               <p class="mt-2 text-lg tracking-wider text-white  ">a legacy of excellence</p>
               <div class="flex">
                 <div class=" max-w-60 mt-5  flex-grow h-1 rounded  bg-[#fb8500]"></div>
-                <p class="text-2xl px-2  italic font-extralight text-[#fb8500] ">view collection</p>
+                <p class="text-2xl px-2  italic font-extralight text-[#fb8500] ">View Car</p>
               </div>
             </div>
           </div>
 
-          <div class="block overflow-hidden bg-cover rounded-lg cursor-pointer h-96 group"
+          <div @click="toggleModal" class="block overflow-hidden bg-cover rounded-lg cursor-pointer h-96 group"
             style="background-image:url('https://images.pexels.com/photos/9452143/pexels-photo-9452143.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')">
             <div
               class="flex flex-col justify-center w-full h-full px-8 pt-60 transition-opacity duration-700 opacity-0 backdrop-blur-sm bg-gray-800/60 group-hover:opacity-100 ">
@@ -117,97 +124,19 @@
               <p class="mt-2 text-lg tracking-wider text-white  ">cutting-edge performance</p>
               <div class="flex">
                 <div class=" max-w-60 mt-5  flex-grow h-1 rounded  bg-[#fb8500]"></div>
-                <p class="text-2xl px-2  italic font-extralight text-[#fb8500] ">view collection</p>
+                <p class="text-2xl px-2  italic font-extralight text-[#fb8500] ">View Car</p>
               </div>
             </div>
           </div>
         </div>
       </div>
 
-
-      <div class="flex items-center justify-center pt-10 -mx-2 sm:mt-0">
-        <button
-          class="text-center min-h-20 px-3 py-2 mx-2 text-2xl font-fontdidot text-white transition ease-in-out  hover:-translate-y-2 duration-300 transform bg-gradient-to-b from-amber-500 to-orange-700 hover:from-white hover:to-white   hover:text-black rounded-sm">
-          Book an Appointment
-        </button>
-        <!-- <button
-          class="bg-transparent border border-black hover:border-transparent text-center min-h-20 px-3 py-2 mx-2 text-2xl font-fontdidot text-black transition ease-in-out  hover:-translate-y-2 duration-300 transform  hover:bg-rose-700 hover:text-white rounded-sm">
-          browse all cars
-        </button> -->
-
-
-      </div>
-
-
-
-
-
-      <!-- ////////////// -->
-
-      <!-- <div class="flex justify-between mt-12 lg:justify-end">
-        <button title="left arrow"
-          class="p-2 text-gray-800 transition-colors duration-300 border rounded-full rtl:-scale-x-100 dark:border-gray-700 dark:text-gray-200 dark:hover:bg-gray-800 hover:bg-gray-100">
-          <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"
-            stroke-width="2">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M15 19l-7-7 7-7" />
-          </svg>
-        </button>
-
-        <button title="right arrow"
-          class="p-2 text-gray-800 transition-colors duration-300 border rounded-full rtl:-scale-x-100 dark:border-gray-700 dark:text-gray-200 dark:hover:bg-gray-800 lg:mx-6 hover:bg-gray-100">
-          <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"
-            stroke-width="2">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7" />
-          </svg>
-        </button>
-      </div> -->
-
     </div>
+    <dialguemodal :modal-active="modalActive" @close-model="toggleModal">
+
+    </dialguemodal>
   </section>
 
-
-  <section>
-    <div class=" h-96  bg-cover rounded-lg cursor-pointer mb-10 ">
-      <!-- <iframe class=" bg-cover" muted width="1281" height="480" src="https://www.youtube.com/embed/fkk211tx07Q"
-        title="Car dealership of prime cars &quot;VipCars&quot; in Dubai commercial promo video" frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe> -->
-      <video autoplay loop muted playsinline src="/assets/video/backgroundvideo.mp4"></video>
-      <div class="justify-center  -mt-40">
-        <h1 class="text-center text-5xl text-white font-serif uppercase ">qwertyuighjk</h1>
-        <h1 class="text-center text-3xl text-white font-serif ">asdfghjklaqwer</h1>
-
-      </div>
-
-
-      <!-- <div class="flex justify-center mt-10">
-        <img class="object-cover w-full h-96 rounded-xl lg:w-4/5"
-          src="https://images.unsplash.com/photo-1556761175-5973dc0f32e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80" />
-      </div> -->
-
-
-
-
-      <!-- <div class="block overflow-hidden bg-cover rounded-lg cursor-pointer h-96 group"
-        style="background-image:url('https://images.pexels.com/photos/9452143/pexels-photo-9452143.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')">
-        <div
-          class="flex flex-col justify-center w-full h-full px-8 pt-60 transition-opacity duration-700 opacity-0 backdrop-blur-sm bg-gray-800/60 group-hover:opacity-100 ">
-          <h2 class="mt-4 text-xl font-semibold text-white uppercase">mclaren</h2>
-          <p class="mt-2 text-lg tracking-wider text-white  ">cutting-edge performance</p>
-          <div class="flex">
-            <div class=" max-w-60 mt-5  flex-grow h-1 rounded  bg-[#fb8500]"></div>
-            <p class="text-2xl px-2  italic font-extralight text-[#fb8500] ">view collection</p>
-          </div>
-        </div>
-      </div> -->
-
-
-
-    </div>
-
-  </section>
-
-  <LayoutServices />
 
   <LayoutFooter />
 
@@ -219,6 +148,16 @@
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
+
+import dialguemodal from './components/dialguemodal.vue';
+
+const modalActive = ref();
+const toggleModal = () => {
+  modalActive.value = !modalActive.value;
+}
+
+
 </script>
 
 <style>
---
+++ components/layout/Hero.vue
@@ -0,0 +1,87 @@
+<template>
+    <header class="bg-white dark:bg-gray-900 pattern">
+        <nav x-data="{ isOpen: false }" class="relative bg-transparent dark:bg-gray-900">
+            <div class="container px-6 py-4 mx-auto md:flex md:justify-between md:items-center">
+                <div class="flex items-center justify-between">
+                    <nuxt-link to="/" class="hover:-translate-y-2 transition ease-in-out duration-300 ">
+                        <img class=" scale-150 w-auto h-6 sm:h-40" src="/assets/imgs/logo2.png" alt="">
+                    </nuxt-link>
+
+                    <!-- Mobile menu button -->
+                    <div class="flex md:hidden">
+                        <button @click="isOpen = !isOpen" type="button"
+                            class="text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400"
+                            aria-label="toggle menu">
+                            <svg @click="!isOpen" xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none"
+                                viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M4 8h16M4 16h16" />
+                            </svg>
+
+                            <!-- <svg @click="isOpen" xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none"
+                                viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                            </svg> -->
+                        </button>
+                    </div>
+                </div>
+
+                <!-- Mobile Menu open: "block", Menu closed: "hidden" -->
+                <div :class="isOpen ? 'block' : 'hidden'"
+                    class="absolute inset-x-0 z-20 w-full px-6 py-4 transition-all duration-300 ease-in-out bg-white dark:bg-gray-900 md:bg-transparent md:dark:bg-transparent md:mt-0 md:p-0 md:top-0 md:relative md:w-auto md:opacity-100 md:translate-x-0 md:flex md:items-center">
+                    <div class="flex flex-col md:flex-row md:mx-6">
+                        <nuxt-link to="#"
+                            class="py-2 px-3 text-[#495057] hover:text-[#fb8500] transition duration-300 font-['ethnocentric_rg'] md:block">Home</nuxt-link>
+                        <nuxt-link to="#"
+                            class="py-2 px-3 text-[#495057] hover:text-[#fb8500] transition duration-300 font-['ethnocentric_rg'] md:block">About</nuxt-link>
+                        <nuxt-link to="#"
+                            class="py-2 px-3 text-[#495057] hover:text-[#fb8500] transition duration-300 font-['ethnocentric_rg'] md:block">Invantory</nuxt-link>
+                        <nuxt-link to="#"
+                            class="py-2 px-3 text-[#495057] hover:text-[#fb8500] transition duration-300 font-['ethnocentric_rg'] md:block">Finance</nuxt-link>
+                    </div>
+
+                    <div class="flex justify-center md:block">
+                        <button
+                            class="relative text-center  px-3 py-2 mx-2 text-lg font-['ethnocentric_rg'] text-white transition ease-in-out  hover:-translate-y-2 duration-300 transform bg-gradient-to-b from-amber-500 to-orange-700 hover:from-white hover:to-white   hover:text-black rounded-sm">Book
+                            an Appointmet</button>
+
+                    </div>
+                </div>
+            </div>
+        </nav>
+
+        <div class="container px-6  mx-auto">
+            <div class="items-center lg:flex">
+                <div class="w-full lg:w-1/2">
+                    <div class="lg:max-w-lg">
+                        <h1 class="text-3xl font-['ethnocentric_rg'] text-gray-800 dark:text-white lg:text-5xl">Best
+                            place to
+                            choose <br> your <span class="text-[#fb8500] ">Service</span></h1>
+
+
+
+                        <button
+                            class="relative text-center mt-10  px-3 py-2 mx-2 text-lg font-['ethnocentric_rg'] text-white transition ease-in-out  hover:-translate-y-2 duration-300 transform bg-gradient-to-b from-amber-500 to-orange-700 hover:from-white hover:to-white   hover:text-black rounded-sm">Book
+                            an Appointmet</button>
+                    </div>
+                </div>
+
+                <div class="flex items-center justify-center w-full mt-6 lg:mt-0 lg:w-1/2">
+
+                    <img class="w-full h-full lg:max-w-3xl motion-safe:animate-bounce" src="/assets/imgs/text.png"
+                        alt="">
+
+                </div>
+            </div>
+        </div>
+    </header>
+</template>
+
+<script setup>
+const isOpen = ref(false);
+</script>
+
+<style>
+.pattern {
+    background-image: url('https://images.pexels.com/photos/5432071/pexels-photo-5432071.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')
+}
+</style>
